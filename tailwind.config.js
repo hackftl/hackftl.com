b567e63f8d9b212f5b9f2264217906a7c83922f8
Matt Watson <matt@devpartners.io>
2020-02-14 21:44:38 -0500

    Initial commit - splash

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      mono: '"Roboto Mono", Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
